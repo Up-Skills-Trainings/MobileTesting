c06fa500307792e037a72522ec6c2cd5c5b6762b
OscarSah <oscar@cybertekschool.com>
2024-01-13 12:20:09 -0500

    created WebTestBase

---
+++ src/test/java/com/cydeo/test/WebTestBase.java
@@ -0,0 +1,38 @@
+package com.cydeo.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class WebTestBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:automationName","UiAutomater2");
+        desiredCapabilities.setCapability("appium:platforName","Android");
+        desiredCapabilities.setCapability("appium:platformVersion","10.0");
+        desiredCapabilities.setCapability("appium:deviceName","Pixel 3");
+        desiredCapabilities.setCapability("appium:browserName","Chrome");
+        URL url;
+        try {
+            url = new URL("http://localhost:4723/");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver = new RemoteWebDriver(url, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
